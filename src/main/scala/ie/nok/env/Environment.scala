5e0074819015186e0783ba59727380b698bc3d63
Philippe Vinchon <p.vinchon@gmail.com>
2023-06-10 15:06:23 +0100

    Add environment detection

---
+++ src/main/scala/ie/nok/env/Environment.scala
@@ -0,0 +1,18 @@
+package ie.nok.env
+
+import zio.{ZIO, System}
+
+enum Environment {
+  case Production
+  case Other
+}
+
+object Environment {
+  val get: ZIO[System, SecurityException, Environment] =
+    System
+      .env("ENV")
+      .map {
+        case Some("production") => Environment.Production
+        case _                  => Environment.Other
+      }
+}
