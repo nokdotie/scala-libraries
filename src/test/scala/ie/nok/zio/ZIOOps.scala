5b6b8f9382ff3c6c3e9cf52e652b2fd5c6479bfa
Philippe Hosey-Vinchon <p.vinchon@gmail.com>
2024-03-06 14:30:09 +0000

    Return A instead of unit (#38)

--- src/test/scala/ie/nok/zio/ZIOOps.scala
+++ src/test/scala/ie/nok/zio/ZIOOps.scala
@@ -3,7 +3,7 @@ package ie.nok.zio
 import zio.{Exit, Runtime, Unsafe, ZIO}
 
 object ZIOOps {
-  def unsafeRun[E <: Throwable, A](a: ZIO[Any, E, A]): Unit =
+  def unsafeRun[E <: Throwable, A](a: ZIO[Any, E, A]): A =
     Unsafe
       .unsafe { implicit unsafe =>
         Runtime.default.unsafe.run(a)
