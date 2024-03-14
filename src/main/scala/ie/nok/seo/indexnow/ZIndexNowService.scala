9a5244953614ac8ea3d5f50d3552721fe7b9188b
Philippe Vinchon <p.vinchon@gmail.com>
2024-03-14 15:41:16 +0000

    Add index now service for Bing and other search engines

---
+++ src/main/scala/ie/nok/seo/indexnow/ZIndexNowService.scala
@@ -0,0 +1,46 @@
+package ie.nok.seo.indexnow
+
+import zio.{ZIO, ZLayer}
+import zio.json.{JsonEncoder, EncoderOps}
+import zio.http.{Body, Client}
+import zio.http.model.{Method, Headers}
+
+trait ZIndexNowService {
+  def submit(urls: Iterable[String]): ZIO[Any, Throwable, Unit]
+}
+
+object ZIndexNowService {
+  def submit(urls: Iterable[String]): ZIO[ZIndexNowService, Throwable, Unit] =
+    ZIO.serviceWithZIO(_.submit(urls))
+}
+
+class ZIndexNowServiceImpl(client: Client) extends ZIndexNowService {
+  private case class Payload(
+      host: String = "https://nok.ie",
+      key: String = IndexNow.key,
+      urlList: Iterable[String]
+  )
+  private given JsonEncoder[Payload] = JsonEncoder.derived[Payload]
+
+  def submit(urls: Iterable[String]): ZIO[Any, Throwable, Unit] = {
+    val payload = Payload(urlList = urls)
+
+    Client
+      .request(
+        "https://api.indexnow.org/indexnow",
+        Method.POST,
+        Headers("content-type", "application/json"),
+        Body.fromString(payload.toJson)
+      )
+      .filterOrDie { _.status.isSuccess } { Throwable("Failed to submit URLs") }
+      .unit
+      .provide(ZLayer.succeed(client))
+  }
+}
+
+object ZIndexNowServiceImpl {
+
+  val layer: ZLayer[Any, Throwable, ZIndexNowService] =
+    Client.default.andTo { ZLayer.fromFunction { ZIndexNowServiceImpl(_) } }
+
+}
