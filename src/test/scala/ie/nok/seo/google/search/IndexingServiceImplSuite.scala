82d2cc717de4fea8dcef30705b9468698d8c8c11
Philippe Vinchon <p.vinchon@gmail.com>
2024-03-16 09:34:18 +0000

    Ignore rate limited tests

--- src/test/scala/ie/nok/seo/google/search/IndexingServiceImplSuite.scala
+++ src/test/scala/ie/nok/seo/google/search/IndexingServiceImplSuite.scala
@@ -3,13 +3,13 @@ package ie.nok.seo.google.search
 class IndexingServiceImplSuite extends munit.FunSuite {
   val instance = IndexingServiceImpl.default.get
 
-  test("IndexingServiceImpl notifies Google's Indexing service of an update") {
+  test("IndexingServiceImpl notifies Google's Indexing service of an update".ignore) {
     instance
       .update("https://nok.ie/example")
       .getOrElse { fail("Failed to update Google Index") }
   }
 
-  test("IndexingServiceImpl notifies Google's Indexing service of a delete") {
+  test("IndexingServiceImpl notifies Google's Indexing service of a delete".ignore) {
     instance
       .delete("https://nok.ie/example")
       .getOrElse { fail("Failed to delete Google Index") }
