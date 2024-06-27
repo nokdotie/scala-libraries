a9ee2385c21f0e285b14812b2fe4c4ca7b52d3a2
Philippe Vinchon <p.vinchon@gmail.com>
2024-06-27 15:35:48 +0100

    Add json codec to direction

--- src/main/scala/ie/nok/unit/Direction.scala
+++ src/main/scala/ie/nok/unit/Direction.scala
@@ -1,6 +1,12 @@
 package ie.nok.unit
 
+import zio.json.{DeriveJsonCodec, JsonCodec}
+
 enum Direction {
   case Ascending
   case Descending
+}
+
+object Direction {
+  given JsonCodec[Direction] = DeriveJsonCodec.gen[Direction]
 }
